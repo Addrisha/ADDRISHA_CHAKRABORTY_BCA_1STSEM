5066ae90af8d13559610e5d652f010ba805bb8f4
Addrisha <144202367+Addrisha@users.noreply.github.com>
2023-10-08 19:25:47 +0530

    Add files via upload

---
+++ INTRO 2 PROGRAMMING/perimeter of rectangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int length,width;
+	printf("enter the length ");
+	scanf("%d",&length);
+	printf("enter the width ");
+	scanf("%d",&width);
+	printf("the perimeter of rectangle is:%d",(length+width)*2);
+	}
