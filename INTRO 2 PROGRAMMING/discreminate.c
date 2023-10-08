b91b8ebc5d5876d7fceed24500708b43906aa128
Addrisha <144202367+Addrisha@users.noreply.github.com>
2023-10-08 20:01:59 +0530

    Add files via upload

---
+++ INTRO 2 PROGRAMMING/discreminate.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("enter the value of a ");
+	scanf("%d",&a);
+	printf("enter the value of b ");
+	scanf("%d",&b);
+	printf("enter the value of c ");
+	scanf("%d",&c);
+	printf("the discreminate of a equation is:%d",((b*b)-(4*a*c)));
+}
