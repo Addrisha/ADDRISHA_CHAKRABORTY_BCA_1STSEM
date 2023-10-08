1714eaecc6f75361d2a2693af42307d263068912
Addrisha <144202367+Addrisha@users.noreply.github.com>
2023-10-08 19:50:51 +0530

    Add files via upload

---
+++ INTRO 2 PROGRAMMING/checking.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	// checking the number is divisible by 2 or not
+	//if the result is 1 then it is divisible by 2
+	//if the result is 0 then it is not divisible by 2
+	int num;
+	printf("enter a num ");
+	scanf("%d",&num);
+	printf("%d",num%2==0);
+	}
