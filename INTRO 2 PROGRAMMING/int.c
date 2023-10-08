e22ff9e746ad550effb4d151cdc768993043cf57
Addrisha <144202367+Addrisha@users.noreply.github.com>
2023-10-08 19:17:35 +0530

    Add files via upload

---
+++ INTRO 2 PROGRAMMING/int.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int side1,side2;
+	printf("enter the first side1 ");
+	scanf("%d",&side1);
+	printf("enter the second side2 ");
+	scanf("%d",&side2);
+	printf("the area of square is:%d",side1*side2);
+}
