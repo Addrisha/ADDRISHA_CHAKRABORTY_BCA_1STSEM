e6fd7ecb180c70b06ae9f39235d02b0fc8dbbe2b
Addrisha <144202367+Addrisha@users.noreply.github.com>
2023-10-08 19:30:53 +0530

    Add files via upload

---
+++ INTRO 2 PROGRAMMING/area of reactangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int length,width;
+	printf("enter the length ");
+	scanf("%d",&length);
+	printf("enter the width ");
+	scanf("%d",&width);
+	printf("the area of rectangle is:%d",length*width);
+}
