f6ecb23d97d0fa76a7b4ac08d5bde4d8ca30c769
Addrisha <144202367+Addrisha@users.noreply.github.com>
2023-10-09 23:12:41 +0530

    Add files via upload

---
+++ INTRO 2 PROGRAMMING/average no.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int num1,num2,num3;
+	printf("enter the first num ");
+	scanf("%d",&num1);
+	printf("enter the second num");
+	scanf("%d",&num2);
+	printf("enter the third num");
+	scanf("%d",&num3);
+	printf("the average of 3 number is: %d",(((num1+num2+num3))/2));
+	}
